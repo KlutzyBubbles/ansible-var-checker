e6f749b82ddc2a837d35240806a89f4cedbbee47
Lee Tzilantonis <lee.tzilantonis@ibm.com>
2021-11-12 16:53:43 +1100

    change coverage output to just html

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 /usr/bin/python3 -m pip install -r requirements.txt
 /usr/bin/python3 -m pip install pytest pytest-cov
-/usr/bin/python3 -m pytest --cov-report xml:avc.xml --cov-report html --cov avc ./tests -vv
+/usr/bin/python3 -m pytest --cov-report html --cov avc ./tests -vv
